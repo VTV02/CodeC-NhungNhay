cdc8b0864c87de84ff9b0f7bdf3e863e24145376
VTV02 <93756924+VTV02@users.noreply.github.com>
2024-06-22 13:51:18 +0700

    Create main.c

---
+++ string/strlen/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+//strlen tinh do dai 1 chuoi dem den khi gap null thi ngung
+
+int main()
+{
+    char str[]="AnhBa";
+    int dodai=strlen(str);
+    printf("%d",dodai);
+    return 0;
+}
